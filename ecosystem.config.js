d26d824d53cce45b1b8d7b7072761a88c718cfaf
sohilpro <sohil123dhh@gmail.com>
2025-11-29 21:17:10 +0330

    fixed any bugs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,37 +1,41 @@
-// ecosystem.config.js
 module.exports = {
   apps: [
     {
       name: "DIV-MAZANDARAN",
-      script: "Divar-MZNDRN/src/index.js",
+      script: "src/index.js", // مسیر نسبی به cwd
+      cwd: "./Divar-MZNDRN", // مهم: تعیین پوشه اجرایی
       instances: 1,
       exec_mode: "fork",
       watch: false,
     },
     {
       name: "DIV-QOM-ARAK",
-      script: "Divar-QOM-ARAK/src/index.js",
+      script: "src/index.js",
+      cwd: "./Divar-QOM-ARAK",
       instances: 1,
       exec_mode: "fork",
       watch: false,
     },
     {
       name: "DIV-SHIRAZ-ISFAHAN",
-      script: "Divar-SHZ-ISFHN/src/index.js",
+      script: "src/index.js",
+      cwd: "./Divar-SHZ-ISFHN",
       instances: 1,
       exec_mode: "fork",
       watch: false,
     },
     {
       name: "DIV-TEHRAN-SEMNAN",
-      script: "Divar-THR-SMNAN/src/index.js",
+      script: "src/index.js",
+      cwd: "./Divar-THR-SMNAN",
       instances: 1,
       exec_mode: "fork",
       watch: false,
     },
     {
-      name: "DIV-YAZD-GILAN",
-      script: "Divar-YAZ-GILAN/src/index.js",
+      name: "DIV-YAZD-GILAN", // اصلاح نام
+      script: "src/index.js",
+      cwd: "./Divar-YAZD-GILAN", // اصلاح مسیر پوشه (اضافه شدن D)
       instances: 1,
       exec_mode: "fork",
       watch: false,
