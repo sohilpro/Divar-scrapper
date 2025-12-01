2f0c7ffcee7b08ad8f7a21f7dba104c970d6a980
sohilpro <sohil123dhh@gmail.com>
2025-12-01 12:47:26 +0330

    sigle process

--- Divar-THR-SMNAN/src/services/scraper.js
+++ Divar-THR-SMNAN/src/services/scraper.js
@@ -38,7 +38,7 @@ class Scraper {
           "--disable-accelerated-2d-canvas",
           "--no-first-run",
           "--no-zygote",
-          "--single-process", // ⚠️ حیاتی: کاهش مصرف رم
+          // "--single-process", // ⚠️ حیاتی: کاهش مصرف رم
           "--disable-gpu",
         ],
         executablePath: "/usr/bin/google-chrome",
